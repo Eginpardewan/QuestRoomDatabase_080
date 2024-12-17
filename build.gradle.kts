38f4b6f1270faba99da5420394394b8f2940ab37
Egin Pardewan <eginpardewan@gmail.com>
2024-12-17 19:26:40 +0700

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.android) version "1.9.10" apply false // Perbarui versi di sini
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 }
