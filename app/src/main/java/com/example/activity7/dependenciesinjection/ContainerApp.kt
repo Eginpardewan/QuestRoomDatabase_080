b21121e2fffd0163d25c52579978dc515d0cac1e
Egin Pardewan <eginpardewan@gmail.com>
2024-12-17 19:27:22 +0700

    update ContainerApp.kt

--- app/src/main/java/com/example/activity7/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/activity7/dependenciesinjection/ContainerApp.kt
@@ -1,20 +1,16 @@
 package com.example.activity7.dependenciesinjection
 
 import android.content.Context
-import com.example.activity7.data.dao.MahasiswaDao
 import com.example.activity7.data.database.KrsDatabase
 import com.example.activity7.repository.LocalRepositoryMhs
 import com.example.activity7.repository.RepositoryMhs
 
-// Interface untuk Dependency Injection
 interface InterfaceContainerApp {
     val repositoryMhs: RepositoryMhs
 }
 
-// Implementasi ContainerApp
 class ContainerApp(private val context: Context) : InterfaceContainerApp {
-    override val repositoryMhs: RepositoryMhs by lazy {
-        val MahasiswaDao: MahasiswaDao = KrsDatabase.getDatabase(context).MahasiswaDao()
-        LocalRepositoryMhs(MahasiswaDao)
+    override  val  repositoryMhs: RepositoryMhs by lazy {
+        LocalRepositoryMhs(KrsDatabase.getDatabase(context).mahasiswa())
     }
 }
