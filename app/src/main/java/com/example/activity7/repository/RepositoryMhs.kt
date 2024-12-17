2b01c2d56e9aef6d4706d5a95a202d0b4d0c76c0
Egin Pardewan <eginpardewan@gmail.com>
2024-12-17 19:41:41 +0700

    update RepositoryMhs.kt

--- app/src/main/java/com/example/activity7/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/activity7/repository/RepositoryMhs.kt
@@ -1,7 +1,18 @@
 package com.example.activity7.repository
 
 import com.example.activity7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    //delete
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    //update
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
